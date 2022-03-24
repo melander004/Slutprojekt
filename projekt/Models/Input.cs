3efba3657447cb7cb2ad1e8d3db25e1a9de126b6
hape04001 <92376988+hape04001@users.noreply.github.com>
2022-03-24 21:35:42 +0100

    Add files via upload

---
+++ projekt/Models/Input.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace sprite_test.Models
+{
+    public class Input
+    {
+        public Keys Up { get; set; } 
+        public Keys Down { get; set; } 
+        public Keys Left { get; set; } 
+        public Keys Right { get; set; } 
+        public Keys Attack { get; set; } 
+    }
+}
