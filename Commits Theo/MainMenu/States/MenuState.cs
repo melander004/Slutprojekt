a6f4f4f8031bf4f084f4049e25ea55e5cb8fef34
thme04001 <91547411+thme04001@users.noreply.github.com>
2022-04-19 15:49:37 +0200

    Update MenuState.cs

--- Commits Theo/MainMenu/States/MenuState.cs
+++ Commits Theo/MainMenu/States/MenuState.cs
@@ -10,82 +10,82 @@ using MainMenu.Controls;
 
 namespace MainMenu.States
 {
-  public class MenuState : State
+  public class MenuState : State 
   {
-    private List<Component> _components;
+        private List<Component> _components;
 
-    public MenuState(Game1 game, GraphicsDevice graphicsDevice, ContentManager content) 
-      : base(game, graphicsDevice, content)
-    {
-      var buttonTexture = _content.Load<Texture2D>("Controls/Button");
-      var buttonFont = _content.Load<SpriteFont>("Fonts/Font");
+        public MenuState(Game1 game, GraphicsDevice graphicsDevice, ContentManager content) 
+        : base(game, graphicsDevice, content)
+        {
+            var buttonTexture = _content.Load<Texture2D>("Controls/Button");
+            var buttonFont = _content.Load<SpriteFont>("Fonts/Font");
 
-      var newGameButton = new Button(buttonTexture, buttonFont)
-      {
-        Position = new Vector2(300, 200),
-        Text = "New Game",
-      };
+            var levelsButton = new Button(buttonTexture, buttonFont)
+            {
+                Position = new Vector2(((Resolution.Width/2)-80), 200),
+                Text = "Levels",
+            };
 
-      newGameButton.Click += NewGameButton_Click;
+            levelsButton.Click += LevelsButton_Click;
 
-      var settingsButton = new Button(buttonTexture, buttonFont)
-      {
-        Position = new Vector2(300, 250),
-        Text = "Settings",
-      };
+            var settingsButton = new Button(buttonTexture, buttonFont)
+            {
+                Position = new Vector2(((Resolution.Width/2)-80), 250),
+                Text = "Settings",
+            };
 
-      settingsButton.Click += SettingsButton_Click;
+            settingsButton.Click += SettingsButton_Click;
 
-      var quitGameButton = new Button(buttonTexture, buttonFont)
-      {
-        Position = new Vector2(300, 300),
-        Text = "Quit Game",
-      };
+            var quitGameButton = new Button(buttonTexture, buttonFont)
+            {
+                Position = new Vector2(((Resolution.Width/2)-80), 300),
+                Text = "Quit Game",
+            };
 
-      quitGameButton.Click += QuitGameButton_Click;
+            quitGameButton.Click += QuitGameButton_Click;
 
-      _components = new List<Component>()
-      {
-        newGameButton,
-        settingsButton,
-        quitGameButton,
-      };
+            _components = new List<Component>()
+            {
+                levelsButton,
+                settingsButton,
+                quitGameButton,
+            };
+        }
+
+        public override void Draw(GameTime gameTime, SpriteBatch spriteBatch)
+        {
+            spriteBatch.Begin();
+
+            foreach (var component in _components)
+                component.Draw(gameTime, spriteBatch);
+
+            spriteBatch.End();
+        }
+
+        private void SettingsButton_Click(object sender, EventArgs e)
+        {
+            Console.WriteLine("Settings");
+        }
+
+        private void LevelsButton_Click(object sender, EventArgs e)
+        {
+            _game.ChangeState(new GameState(_game, _graphicsDevice, _content));
+        }
+
+        public override void PostUpdate(GameTime gameTime)
+        {
+            // remove sprites if they're not needed
+        }
+
+        public override void Update(GameTime gameTime)
+        {
+            foreach (var component in _components)
+                component.Update(gameTime);
+        }
+
+        private void QuitGameButton_Click(object sender, EventArgs e)
+        {
+            _game.Exit();
+        }
     }
-
-    public override void Draw(GameTime gameTime, SpriteBatch spriteBatch)
-    {
-      spriteBatch.Begin();
-
-      foreach (var component in _components)
-        component.Draw(gameTime, spriteBatch);
-
-      spriteBatch.End();
-    }
-
-    private void SettingsButton_Click(object sender, EventArgs e)
-    {
-      Console.WriteLine("Settings");
-    }
-
-    private void NewGameButton_Click(object sender, EventArgs e)
-    {
-      _game.ChangeState(new GameState(_game, _graphicsDevice, _content));
-    }
-
-    public override void PostUpdate(GameTime gameTime)
-    {
-      // remove sprites if they're not needed
-    }
-
-    public override void Update(GameTime gameTime)
-    {
-      foreach (var component in _components)
-        component.Update(gameTime);
-    }
-
-    private void QuitGameButton_Click(object sender, EventArgs e)
-    {
-      _game.Exit();
-    }
-  }
 }
