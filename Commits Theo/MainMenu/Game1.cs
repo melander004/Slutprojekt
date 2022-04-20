344806a732f7b098f50aefd86e36f55b864f799b
thme04001 <91547411+thme04001@users.noreply.github.com>
2022-04-20 21:46:29 +0200

    Add files via upload

---
+++ Commits Theo/MainMenu/Game1.cs
@@ -0,0 +1,85 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Media;
+using MainMenu.States;
+
+namespace MainMenu
+{
+    public class Game1 : Game
+    {
+        private GraphicsDeviceManager _graphics;
+
+        private SpriteBatch _spriteBatch;
+
+        private State _currentState;
+        private State _nextState;
+        public void ChangeState(State state)
+        {
+            _nextState = state;
+        }
+
+        Song mainMenuMusic;
+
+        public Game1()
+        {
+            _graphics = new GraphicsDeviceManager(this);
+            _graphics.PreferredBackBufferWidth = Resolution.Width;
+            _graphics.PreferredBackBufferHeight = Resolution.Height;
+            Content.RootDirectory = "Content";
+            IsMouseVisible = true;
+        }
+
+        protected override void Initialize()
+        {
+            IsMouseVisible = true;
+
+            base.Initialize();
+        }
+
+        protected override void LoadContent()
+        {
+            _spriteBatch = new SpriteBatch(GraphicsDevice);
+
+            _currentState = new MenuState(this, _graphics.GraphicsDevice, Content);
+
+            mainMenuMusic = this.Content.Load<Song>("Audio/Meny");
+            MediaPlayer.Play(mainMenuMusic);
+            MediaPlayer.MediaStateChanged += MediaPlayer_MediaStateChanged;
+        }
+
+        void MediaPlayer_MediaStateChanged(object sender, System.EventArgs e)
+        {
+            MediaPlayer.Volume -= -0.1f;  
+            MediaPlayer.Play(mainMenuMusic);
+        }
+
+        protected override void Update(GameTime gameTime)
+        {
+            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
+                Exit();
+
+            if(_nextState != null)
+            {
+                _currentState = _nextState;
+
+                _nextState = null;
+            }
+
+            _currentState.Update(gameTime);
+
+            _currentState.PostUpdate(gameTime);
+
+            base.Update(gameTime);
+        }
+
+        protected override void Draw(GameTime gameTime)
+        {
+            GraphicsDevice.Clear(Color.SlateGray);
+
+            _currentState.Draw(gameTime, _spriteBatch);
+
+            base.Draw(gameTime);
+        }
+    }
+}
