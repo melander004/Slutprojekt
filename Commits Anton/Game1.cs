60a2fd1757ee845103f1f4c8fe07e47346ae4cb3
anol04002 <61683082+anol04002@users.noreply.github.com>
2022-04-20 21:43:35 +0200

    Add files via upload

---
+++ Commits Anton/Game1.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Media;
+
+namespace ännu_ett_nytt_slutprojekt
+{
+
+    public class Game1 : Game
+    {
+        private GraphicsDeviceManager _graphics;
+        private SpriteBatch _spriteBatch;
+        Song Naboo_music;
+
+        Texture2D Bakgrund;
+        Vector2 Bakgrundpos;
+
+        
+
+        Floor tile;        
+
+        public Game1()
+        {
+            _graphics = new GraphicsDeviceManager(this);
+            Content.RootDirectory = "Content";
+            IsMouseVisible = true;
+        }
+
+        protected override void Initialize()
+        {
+
+            tile = new Floor();
+            // TODO: Add your initialization logic here
+            tile.Initialize(Content);
+            base.Initialize();
+        }
+
+        protected override void LoadContent()
+        {
+            _spriteBatch = new SpriteBatch(GraphicsDevice);
+
+            Bakgrund = this.Content.Load<Texture2D>("Naboo_bakgrund");
+            Bakgrundpos = new Vector2(0,0);
+            Naboo_music = this.Content.Load<Song>("Naboo");
+            MediaPlayer.Play(Naboo_music);
+            MediaPlayer.MediaStateChanged += MediaPlayer_MediaStateChanged;
+
+            // TODO: use this.Content to load your game content here
+
+        }
+        void MediaPlayer_MediaStateChanged(object sender, System.EventArgs e)
+        {
+            MediaPlayer.Volume -= -0.1f;
+            MediaPlayer.Play(Naboo_music);
+        }
+        protected override void Update(GameTime gameTime)
+        {
+            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
+                Exit();
+
+            // TODO: Add your update logic here
+
+            base.Update(gameTime);
+        }
+
+        protected override void Draw(GameTime gameTime)
+        {
+            GraphicsDevice.Clear(Color.SlateGray);
+
+            // TODO: Add your drawing code here
+            _spriteBatch.Begin();
+            _spriteBatch.Draw(Bakgrund, Bakgrundpos, Color.White);
+            tile.Draw(_spriteBatch);
+            _spriteBatch.End();
+
+            base.Draw(gameTime);
+        }
+    }
+}
