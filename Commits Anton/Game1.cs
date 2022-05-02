66e29c515cfc6619346d73f8b6cf0e2f5987eef7
anol04002 <61683082+anol04002@users.noreply.github.com>
2022-05-02 10:14:06 +0200

    Update Game1.cs

--- Commits Anton/Game1.cs
+++ Commits Anton/Game1.cs
@@ -1,36 +1,27 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-using System.Linq;
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
-using Microsoft.Xna.Framework.Media;
 using Slutprojekt.States;
+using Microsoft.Xna.Framework.Media;
 
 namespace Slutprojekt
 {
-
     public class Game1 : Game
     {
         private GraphicsDeviceManager _graphics;
+
         private SpriteBatch _spriteBatch;
 
         private State _currentState;
-
         private State _nextState;
-
+            
         public void ChangeState(State state)
         {
             _nextState = state;
         }
+
         Song Naboo_music;
-
-        Texture2D Bakgrund;
-        Vector2 Bakgrundpos;
-
-        Floor tile;        
-
+        
         public Game1()
         {
             _graphics = new GraphicsDeviceManager(this);
@@ -40,32 +31,24 @@ namespace Slutprojekt
 
         protected override void Initialize()
         {
-
-            tile = new Floor();
             // TODO: Add your initialization logic here
-            tile.Initialize(Content);
+
+            IsMouseVisible = true;
+
             base.Initialize();
         }
 
         protected override void LoadContent()
         {
             _spriteBatch = new SpriteBatch(GraphicsDevice);
-
-            _currentState = new MenuState(this, _graphics.GraphicsDevice, Content);
-
-            Bakgrund = this.Content.Load<Texture2D>("Naboo_bakgrund2");
-            Bakgrundpos = new Vector2(0,0);
             Naboo_music = this.Content.Load<Song>("Naboo");
-            MediaPlayer.Play(Naboo_music);
-            MediaPlayer.Volume = MusicVolume.Volume;
-
             // TODO: use this.Content to load your game content here
-
+            _currentState = new MenuState(this, _graphics.GraphicsDevice, Content);
         }
-        
+
         protected override void Update(GameTime gameTime)
         {
-            if (Keyboard.GetState().IsKeyDown(Keys.Escape))
+            if(Keyboard.GetState().IsKeyDown(Keys.Escape))
                 ChangeState(new MenuState(this, _graphics.GraphicsDevice, Content));
 
             if(_nextState != null)
@@ -83,24 +66,18 @@ namespace Slutprojekt
             _graphics.PreferredBackBufferHeight = Resolution.Height;
             _graphics.ApplyChanges();
 
-
-            // TODO: Add your update logic here
+            MediaPlayer.Play(Naboo_music);
+            MediaPlayer.Volume = MusicVolume.Volume;
 
             base.Update(gameTime);
         }
 
         protected override void Draw(GameTime gameTime)
         {
+            // TODO: Add your drawing code here
             GraphicsDevice.Clear(Color.SlateGray);
 
-            // TODO: Add your drawing code here
-            _spriteBatch.Begin();
-
             _currentState.Draw(gameTime, _spriteBatch);
-            
-
-
-            _spriteBatch.End();
 
             base.Draw(gameTime);
         }
