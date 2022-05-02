71a1517ba13814102804bec05fc8d8aa944d6078
thme04001 <91547411+thme04001@users.noreply.github.com>
2022-05-02 10:07:03 +0200

    Update Resolution.cs

--- Commits Theo/MainMenu/Resolution.cs
+++ Commits Theo/MainMenu/Resolution.cs
@@ -3,6 +3,9 @@ namespace MainMenu
     public class Resolution
     {
         public static int Width = 800;
+        
         public static int Height = 800;
+
+        public static float Scale = 1f;
     }
 }
