bab8f743ae44216fbc50e8907e95de10d6c79447
thme04001 <91547411+thme04001@users.noreply.github.com>
2022-05-06 15:04:13 +0200

    Update LevelState.cs

--- Commits Theo/MainMenu/States/LevelState.cs
+++ Commits Theo/MainMenu/States/LevelState.cs
@@ -6,65 +6,98 @@ using System.Threading.Tasks;
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Content;
 using Microsoft.Xna.Framework.Graphics;
+using MainMenu.Controls;
+using MainMenu;
 
 namespace MainMenu.States
 {
-  public class LevelState : State
-  {
-    Texture2D Bakgrund1;
-
-    Texture2D Bakgrund2;
-    
-    Vector2 BakgrundPos;
-
-    Floor tile; 
-    
-    public LevelState(Game1 game, GraphicsDevice graphicsDevice, ContentManager content) 
-      : base(game, graphicsDevice, content)
+    public class LevelState : State
     {
-      tile = new Floor();
-      tile.Initialize(content);
-      
-      Bakgrund1 = _game.Content.Load<Texture2D>("Textures/Naboo_Bakgrund");
-      Bakgrund2 = _game.Content.Load<Texture2D>("Textures/Naboo_Bakgrund(16 9)");
-      BakgrundPos = new Vector2(0,0);
+        private List<Component> _components;
+
+        public LevelState(Game1 game, GraphicsDevice graphicsDevice, ContentManager content) 
+        : base(game, graphicsDevice, content)
+        {
+            var buttonTexture = _content.Load<Texture2D>("Controls/Button");
+            var buttonFont = _content.Load<SpriteFont>("Fonts/Font");
+
+            var nabooButton = new Button(buttonTexture, buttonFont)
+            {
+              Position = new Vector2(Resolution.Width/4-80, Resolution.Height/2-20),
+              Text = "Naboo",
+            };
+
+            nabooButton.Click += NabooButton_Click;
+
+            var coruscantButton = new Button(buttonTexture, buttonFont)
+            {
+              Position = new Vector2(Resolution.Width/2-80, Resolution.Height/2-20),
+              Text = "Coruscant",
+            };
+
+            coruscantButton.Click += CoruscantButton_Click;
+
+            var mustafarButton = new Button(buttonTexture, buttonFont)
+            {
+              Position = new Vector2(Resolution.Width/1.333333333333333333f-80, Resolution.Height/2-20),
+              Text = "Mustafar",
+            };
+
+            mustafarButton.Click += MustafarButton_Click;
+
+            var backButton = new Button(buttonTexture, buttonFont)
+            {
+                Position = new Vector2(Resolution.Width/20, Resolution.Height/1.1f),
+                Text = "Back",
+            };
+
+            backButton.Click += BackButton_Click;
+
+            _components = new List<Component>()
+            {
+                nabooButton,
+                mustafarButton,
+                coruscantButton,
+                backButton,
+            };
+        }
+        public override void Draw(GameTime gameTime, SpriteBatch spriteBatch)
+        {
+            spriteBatch.Begin();
+
+            foreach (var component in _components)
+                component.Draw(gameTime, spriteBatch);
+
+            spriteBatch.End();
+        }
+
+        private void NabooButton_Click(object sender, EventArgs e)
+        {
+          _game.ChangeState(new NabooState(_game, _graphicsDevice, _content));
+        }
+
+        private void MustafarButton_Click(object sender, EventArgs e)
+        {
+          _game.ChangeState(new MustafarState(_game, _graphicsDevice, _content));
+        }
+
+        private void CoruscantButton_Click(object sender, EventArgs e)
+        {
+          _game.ChangeState(new CoruscantState(_game, _graphicsDevice, _content));
+        }
+        private void BackButton_Click(object sender, EventArgs e)
+        {
+            _game.ChangeState(new MenuState(_game, _graphicsDevice, _content));
+        }
+
+        public override void PostUpdate(GameTime gameTime)
+        {
+           
+        }
+        public override void Update(GameTime gameTime)
+        {
+            foreach (var component in _components)
+                component.Update(gameTime);
+        }
     }
-
-    public override void Draw(GameTime gameTime, SpriteBatch spriteBatch)
-    {
-      spriteBatch.Begin();
-
-      if(Resolution.Width == 800){
-      spriteBatch.Draw(Bakgrund1, BakgrundPos, Color.White);
-      tile.Draw(spriteBatch);
-      }      
-
-      if(Resolution.Width == 1280){
-      spriteBatch.Draw(Bakgrund2, BakgrundPos, null, Color.White, 0f, Vector2.Zero, 1f, SpriteEffects.None, 0f);
-      tile.Draw(spriteBatch);
-      }
-
-      if(Resolution.Width == 1600){
-      spriteBatch.Draw(Bakgrund2, BakgrundPos, null, Color.White, 0f, Vector2.Zero, 1.25f, SpriteEffects.None, 0f);
-      tile.Draw(spriteBatch);
-      }
-
-      if(Resolution.Width == 1920){
-      spriteBatch.Draw(Bakgrund2, BakgrundPos, null, Color.White, 0f, Vector2.Zero, 1.5f, SpriteEffects.None, 0f);
-      tile.Draw(spriteBatch);
-      } 
-
-      spriteBatch.End();
-    }
-
-    public override void PostUpdate(GameTime gameTime)
-    {
-
-    }
-
-    public override void Update(GameTime gameTime)
-    {
-
-    }
-  }
 }
