fb4c57d1c75872966e72c7d9a4c01aa82fca80a7
hape04001 <92376988+hape04001@users.noreply.github.com>
2022-05-10 14:45:17 +0200

    Add files via upload

---
+++ Commits Hampus/AnimationClass/Models/Input.cs
@@ -0,0 +1,28 @@
+using Microsoft.Xna.Framework.Input;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AnimationClass.Models
+{
+  public class Input
+  {
+    public Keys Down { get; set; }
+
+    public Keys Left { get; set; }
+
+    public Keys Right { get; set; }
+
+    public Keys Up { get; set; }
+
+    public Keys Attack{ get; set; }
+
+    public Keys currentkey {get; set;}
+    public Keys lastkey {get; set;}
+
+   
+
+  }
+}
