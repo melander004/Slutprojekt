cc055fceed60290e1764c4b7efba4b2c9d2eb351
thme04001 <91547411+thme04001@users.noreply.github.com>
2022-05-24 15:31:01 +0200

    Add files via upload

--- Slutprojekt/Game1.cs
+++ Slutprojekt/Game1.cs
@@ -1,14 +1,26 @@
 ﻿using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Media;
+using Slutprojekt.States;
 
 namespace Slutprojekt
 {
     public class Game1 : Game
     {
         private GraphicsDeviceManager _graphics;
+
         private SpriteBatch _spriteBatch;
 
+        private State _currentState;
+        
+        private State _nextState;
+        
+        public void ChangeState(State state)
+        {
+            _nextState = state;            
+        }
+        
         public Game1()
         {
             _graphics = new GraphicsDeviceManager(this);
@@ -20,6 +32,8 @@ namespace Slutprojekt
         {
             // TODO: Add your initialization logic here
 
+            IsMouseVisible = true;
+
             base.Initialize();
         }
 
@@ -28,23 +42,40 @@ namespace Slutprojekt
             _spriteBatch = new SpriteBatch(GraphicsDevice);
 
             // TODO: use this.Content to load your game content here
+            _currentState = new MenuState(this, _graphics.GraphicsDevice, Content);
         }
 
         protected override void Update(GameTime gameTime)
         {
-            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
-                Exit();
+            if(Keyboard.GetState().IsKeyDown(Keys.Escape))
+                ChangeState(new MenuState(this, _graphics.GraphicsDevice, Content));
 
-            // TODO: Add your update logic here
+            if(_nextState != null)
+            {
+                _currentState = _nextState;
+
+                _nextState = null;
+            }
+
+            _currentState.Update(gameTime);
+
+            _currentState.PostUpdate(gameTime);
+
+            _graphics.PreferredBackBufferWidth = Resolution.Width;
+            _graphics.PreferredBackBufferHeight = Resolution.Height;
+            _graphics.ApplyChanges();
+
+            MediaPlayer.Volume = Music.Volume;
 
             base.Update(gameTime);
         }
 
         protected override void Draw(GameTime gameTime)
         {
-            GraphicsDevice.Clear(Color.CornflowerBlue);
-
             // TODO: Add your drawing code here
+            GraphicsDevice.Clear(Color.SlateGray);
+
+            _currentState.Draw(gameTime, _spriteBatch);
 
             base.Draw(gameTime);
         }
