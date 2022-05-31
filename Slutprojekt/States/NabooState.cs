89dbbbcdeeb5bbff98866718803ca8ce45fae158
thme04001 <91547411+thme04001@users.noreply.github.com>
2022-05-31 15:50:40 +0200

    Add files via upload

--- Slutprojekt/States/NabooState.cs
+++ Slutprojekt/States/NabooState.cs
@@ -26,9 +26,11 @@ namespace Slutprojekt.States
 
     private List<Sprite> _sprites;
 
-    Texture2D Enemy;
+    Texture2D enemy;
+
     public Rectangle enemyRect = new Rectangle(500, Resolution.Height-105, 84, 58);
   
+    // Loads textures, music, animations and positions for the state.
     public NabooState(Game1 game, GraphicsDevice graphicsDevice, ContentManager content) 
       : base(game, graphicsDevice, content)
     {
@@ -42,16 +44,18 @@ namespace Slutprojekt.States
       DOF = _game.Content.Load<Song>("Audio/Naboo");
       MediaPlayer.Play(DOF);
 
-      Enemy = _game.Content.Load<Texture2D>("Enemy/darthmaulAFK");
+      enemy = _game.Content.Load<Texture2D>("Enemy/darthmaulAFK");
 
-      var animations = new Dictionary<string, Animation>(){
+      var animations = new Dictionary<string, Animation>()
+      {
         { "PlayerRight", new Animation(_game.Content.Load<Texture2D>("Player/ObiwanWalk"), 12) },
         { "PlayerLeft", new Animation(_game.Content.Load<Texture2D>("Player/ObiwanRev"), 12) },
         { "PlayerAttack", new Animation(_game.Content.Load<Texture2D>("Player/ObiwanAtt"), 3) },
         { "PlayerIdle", new Animation(_game.Content.Load<Texture2D>("Player/ObiwanAFK"), 1) },
       };
 
-      _sprites = new List<Sprite>(){
+      _sprites = new List<Sprite>()
+      {
         new Sprite(new Dictionary<string, Animation>(){
           { "PlayerRight", new Animation(_game.Content.Load<Texture2D>("Player/ObiwanWalk"), 12) },
           { "PlayerLeft", new Animation(_game.Content.Load<Texture2D>("Player/ObiwanRev"), 12) },
@@ -76,30 +80,29 @@ namespace Slutprojekt.States
     {
       spriteBatch.Begin();
 
-      if(Resolution.Width == 800){
+      // Draws the default Naboo background (800x800 px).
+      if(Resolution.Width == 800)
         spriteBatch.Draw(defaultBackground, backgroundPos, Color.White);
-      }      
 
-      if(Resolution.Width == 1280){
+      // Draws the 16:9 version of the Naboo background and scales it to the correct resolution. (1f, 1.25f and 1.5f)
+      if(Resolution.Width == 1280)
         spriteBatch.Draw(background, backgroundPos, null, Color.White, 0f, Vector2.Zero, 1f, SpriteEffects.None, 0f);
-      }
 
-      if(Resolution.Width == 1600){
+      if(Resolution.Width == 1600)
         spriteBatch.Draw(background, backgroundPos, null, Color.White, 0f, Vector2.Zero, 1.25f, SpriteEffects.None, 0f);
-      }
 
-      if(Resolution.Width == 1920){
+      if(Resolution.Width == 1920)
         spriteBatch.Draw(background, backgroundPos, null, Color.White, 0f, Vector2.Zero, 1.5f, SpriteEffects.None, 0f);
-      }
 
-      for (int i = 0; i<100; i++){
-          spriteBatch.Draw(nabooFloor, new Vector2((int)nabooFloorPos.X + (i*nabooFloor.Width), (int)nabooFloorPos.Y), Color.White);
-      } 
+      // Draws the Naboo floor texture, "i<100" means that the texture is loaded 99 times because of the different resolutions.
+      for (int i = 0; i<100; i++)
+        spriteBatch.Draw(nabooFloor, new Vector2((int)nabooFloorPos.X + (i*nabooFloor.Width), (int)nabooFloorPos.Y), Color.White);
 
+      // Draws the sprites that should be shown on the screen.
       foreach (var sprite in _sprites)
         sprite.Draw(spriteBatch);
     
-      spriteBatch.Draw(Enemy, enemyRect, Color.White);
+      spriteBatch.Draw(enemy, enemyRect, Color.White);
 
       spriteBatch.End();
     }
@@ -111,6 +114,7 @@ namespace Slutprojekt.States
 
     public override void Update(GameTime gameTime)
     {
+      // Updates the sprites position and animation.
       foreach (var sprite in _sprites)
         sprite.Update(gameTime, _sprites);
     }
